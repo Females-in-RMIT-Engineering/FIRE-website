145a28ca5c18829032f11e97222f3f95e11ee994
Kent-Go <kentgo09@gmail.com>
2024-05-04 20:57:07 +1000

    update sponsors for 2024

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -4,13 +4,10 @@ import React from 'react';
 import { Helmet } from "react-helmet";
 import "./pagesCSS/sponsors.css";
 
-import JohnHollandLogo from './pagesResources/logos/JohnHollandLogo.png';
-import AmazonLogo from './pagesResources/logos/AmazonLogo.png';
-import StantecLogo from './pagesResources/logos/StantecLogo.png';
-import SystraLogo from './pagesResources/logos/SystraLogo.png';
-import TelstraLogo from './pagesResources/logos/TelstraLogo.png';
-import APALogo from './pagesResources/logos/APALogo.png';
-import AECOMLogo from './pagesResources/logos/AECOMLogo.png';
+import lendleaseLogo from './pagesResources/logos/lendleaseLogo.png';
+import pwcLogo from './pagesResources/logos/pwcLogo.png';
+import ARCADISLogo from './pagesResources/logos/ARCADISLogo.png';
+import JacobsLogo from './pagesResources/logos/JacobsLogo.png';
 
 const Sponsors = () => {
     return (
@@ -30,38 +27,27 @@ const Sponsors = () => {
 
           <h2> Platinum </h2>
             <div class = 'SponsorRow'>
-              <div class = 'SpnsorColumn'>
-                <img src={JohnHollandLogo} alt = 'John Holland Logo' width = {270} height = {90} class = 'SponsorLogos' />
+              <div class = 'SponsorColumn'>
+                <img src={lendleaseLogo} alt = 'lendlease Logo' height = {190} class = 'SponsorLogos'/>
+                {/* lendlease description adapted from https://www.lendlease.com/au/about-us/ */}
+                <p>As a global, integrated real estate group, we draw on decades of experience and the latest thinking in investments, development, construction, design and placemaking to deliver iconic and enormously successful places.</p>
               </div>
             </div>
 
           <h2> Gold </h2>
             <div class = 'SponsorRow'>
-              <div class = 'SpnsorColumn'>
-                <img src={AmazonLogo} alt = 'Amazon Logo' width = {270} height = {90} class = 'SponsorLogos' />
-              </div>
-              <div class = 'SpnsorColumn'>
-                <img src={StantecLogo} alt = 'Stantec Logo' width = {270} height = {90} class = 'SponsorLogos' />
-              </div>
-            </div>
-            <div class = 'SponsorRow'>
-              <div class = 'SpnsorColumn'>
-                <img src={SystraLogo} alt = 'Systra Logo' width = {270} height = {90} class = 'SponsorLogos' />
+              <div class = 'SponsorColumn'>
+                <img src={pwcLogo} alt = 'pwc Logo' width = {200} class = 'SponsorLogos' />
               </div>
             </div>
 
           <h2> Silver </h2>
-            <div class = 'SponsorRow'>
-              <div class = 'SpnsorColumn'>
-                <img src={TelstraLogo} alt = 'Telstra Logo' width = {270} height = {90} class = 'SponsorLogos' />
+            <div class = 'SponsorRowGrid'>
+              <div class = 'SponsorColumn'>
+                <img src={ARCADISLogo} alt = 'ARCADIS Logo' width = {270} class = 'SponsorLogos' />
               </div>
-              <div class = 'SpnsorColumn'>
-                <img src={APALogo} alt = 'APA Logo' width = {270} height = {90} class = 'SponsorLogos' />
-              </div>
-            </div>
-            <div class = 'SponsorRow'>
-              <div class = 'SpnsorColumn'>
-                <img src={AECOMLogo} alt = 'Aecome Logo' width = {270} height = {90} class = 'SponsorLogos' />
+              <div class = 'SponsorColumn'>
+                <img src={JacobsLogo} alt = 'Jacobs Logo' width = {270} class = 'SponsorLogos' />
               </div>
             </div>
 
